e37a12b95f2a3cd768c9c1e432c13e0e94fd3bf2
Penny <xyang16@wou.edu>
2019-10-22 21:42:00 -0700

    test

---
+++ HW3/IQueueInterface.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// This is the Interface class for the LinkedQueue that we will use in the LinkedQueue class.
+/// </summary>
+namespace _460HW3
+{
+    /// <summary>
+    ///  A FIFO queue interface.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface IQueueInterface<T>
+    {
+        /// <summary>
+        /// This method will push or add an element to the list.
+        /// <param name="element"></param> this is the element being appended.
+        /// <returns> return the element that was enqueued</returns>
+        /// </summary>
+        T Push(T element);
+
+        /// <summary>
+        ///  this function shows what is on top of the list.
+        /// </summary>
+        /// <returns>Remove and return the front element.|Returns the object on top or recently appended</returns>
+        T Pop();
+
+
+        /// <summary>
+        /// Return but don't remove the front element.
+        /// </summary>
+    
+        T Peek();
+
+
+        /// <summary>
+        ///  This method returns a bool (true or false) depending on if the the list is empty or not
+        ///  True if it exits an element ,false if the list is empty.
+        /// </summary>
+        /// <returns></returns>
+        bool IsEmpty();
+
+    }
+}
+
+
---
+++ HW3/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _460HW3
+{
+    class Node<T>
+    {
+        public T data; //This is the data that is in the Node
+        public Node<T> next; // This points to the next Node
+
+        /// <summary>
+        ///This is the default constructor of the Node Class
+        ///<paramref name="data"/>Object that is held within the node
+        ///<paramref name="next"/>Pointer to the node next to it
+        /// </summary>
+        /// 
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+    }
+}
