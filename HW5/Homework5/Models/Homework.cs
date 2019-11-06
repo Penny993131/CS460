b83ff8be028829723733451d54dbabeebeab0bd6
Penny <xyang16@wou.edu>
2019-11-05 17:30:54 -0800

    this is my first commit of Homework.cs but I haven't added all the entites

---
+++ HW5/Homework5/Models/Homework.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Homework5.Models
+{
+    public class Homework
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+       
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {Priority} {LastName} {DOB} {Age}";
+        }
+    }
+}
