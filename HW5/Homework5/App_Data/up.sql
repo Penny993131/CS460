0c284f07cef8b57143e791413c164c5a6adbd451
Penny <xyang16@wou.edu>
2019-11-05 18:24:36 -0800

    update of up.sql but it's still not quite working

--- HW5/Homework5/App_Data/up.sql
+++ HW5/Homework5/App_Data/up.sql
@@ -1,20 +1,20 @@
 ﻿CREATE TABLE [dbo].[Homeworks]
 (
 	[ID]		INT IDENTITY (1,1)	NOT NULL,
-	[Priority]  NVARCHAR(64)            NOT NULL,
-	[DueDate]   DATETIME		        NOT NULL,
+	[ThePriority]  INT                 NOT NULL,
+	[DueDate]   DATE		        NOT NULL,
 	[DueTime]	TIME		        NOT NULL,
-	[Department] TEXT			    NOT NULL,
+	[Department] TEXT	            NOT NULL,
 	[Course#]    TEXT               NOT NULL,
-	[Homework Title] TEXT           NOT NULL,
-	[Notes]      TEXT               NOT NULL,
+	[HomeworkTitle] TEXT            NOT NULL,
+	[Notes]     TEXT                 NOT NULL,
 	CONSTRAINT [PK_dbo.Homeworks] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
-INSERT INTO [dbo].[Homeworks] (Priority, DueDate, DueTime,Department,Course#,HomeworkTitle,Notes) VALUES
-	('1','11-01-2019','23:59','Math','Complete Graph','  '),
-	('2','12-31-2019','23:59''CS''Photoshop','  '),
-	('3','09-15-2018','23:59''Art''Oil Painting','  '),
-	('2''10-01-2018','23:59''Music''Broadway Music','  '),	
-	('1''08-08-2019','23:59''Education''Education Setting','  ')
+INSERT INTO [dbo].[Homeworks] (ThePriority, DueDate, DueTime, Department, Course#, HomeworkTitle,Notes) VALUES
+	(1,'11-01-2019','23:59','Math','MTH354','Complete Graph', 'Graded')
+	(2,'12-31-2019','23:59','CS','CS121','Photoshop','unGraded'),
+	(3,'09-15-2018','23:59','Art','Art231','Oil Painting','Graded'),
+	(2,'10-01-2018','23:59''Music','Music110','Broadway Music','unGraded'),	
+	(1,'08-08-2019','23:59','Education','ED380','Education Setting','Graded')
 GO
