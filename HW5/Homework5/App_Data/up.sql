f45303552052abfd7a910b3db31f661508ec4618
Penny <xyang16@wou.edu>
2019-11-05 17:21:29 -0800

    first update of up.sql and down sql to make sure my database is working

---
+++ HW5/Homework5/App_Data/up.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Homeworks]
+(
+	[ID]		INT IDENTITY (1,1)	NOT NULL,
+	[Priority]  NVARCHAR(64)            NOT NULL,
+	[DueDate]   DATETIME		        NOT NULL,
+	[DueTime]	TIME		        NOT NULL,
+	[Department] TEXT			    NOT NULL,
+	[Course#]    TEXT               NOT NULL,
+	[Homework Title] TEXT           NOT NULL,
+	[Notes]      TEXT               NOT NULL,
+	CONSTRAINT [PK_dbo.Homeworks] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+INSERT INTO [dbo].[Homeworks] (Priority, DueDate, DueTime,Department,Course#,HomeworkTitle,Notes) VALUES
+	('1','11-01-2019','23:59','Math','Complete Graph','  '),
+	('2','12-31-2019','23:59''CS''Photoshop','  '),
+	('3','09-15-2018','23:59''Art''Oil Painting','  '),
+	('2''10-01-2018','23:59''Music''Broadway Music','  '),	
+	('1''08-08-2019','23:59''Education''Education Setting','  ')
+GO
