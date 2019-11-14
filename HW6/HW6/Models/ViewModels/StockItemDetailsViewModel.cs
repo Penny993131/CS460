5c1ea2e8fd36c7d4d391696d195dfabb5018603e
Penny <xyang16@wou.edu>
2019-11-13 22:44:38 -0800

    create StockItemsController and ViewModel class to build business logics to pass into view

---
+++ HW6/HW6/Models/ViewModels/StockItemDetailsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6.Models.ViewModels
+{
+    public class StockItemDetailsViewModel(StockItem stockitem)
+    {
+        StockItemID = stockitem.StockItemID;
+        StockItemName = stockitem.Name;
+        StockItemSize = 
+    }
+
+
+public string StockItemName { get; private set; }
+public decimal UnitPrice { get; private set; }
+public string Size { get; private set; }
+public decimal TypicalWeightPerUnit { get; private set; }
+public int LeadTimeDays { get; private set; }
+public DateTime ValidFrom { get; private set; }
+public string Tags { get; private set; }
+
+
+
+
+}
