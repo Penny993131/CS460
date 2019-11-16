c522a86aa650de727ee2ff196c0cd90e91db591d
Penny <xyang16@wou.edu>
2019-11-15 19:13:27 -0800

    Fix my homecontroller and pass the correct parameter'search' in my ActionResult, make sure my seach function work. Also Fix my Home/Index.cshtml to match the parameter in ActionResult and make sure I have the ViewBag.Sucees in both here and controller. Fix my ActionLink and matched the page to the correct controller/ Using the knowledge from HW5

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -11,13 +11,11 @@ namespace HW6.Controllers
     public class HomeController : Controller
     {
         private WWIContext db = new WWIContext();
-        public ActionResult Index()
-        {
+       
+        // I intially put my return viewn in the StockItemsController which is the wrong controller.
+        //This return will not do anything in stockitem controller because this is the initial page (When we first visit the site)
+        //return View(db.StockItems.Where(s => s.StockItemName.Contains(name)).ToList());
 
-            //This return will not do anything because this is the initial page (When we first visit the site)
-            //return View(db.StockItems.Where(s => s.StockItemName.Contains(name)).ToList());
-            return View();
-        }
         /*
          * PROBLEM: When typing something in the search and clicking the search button, the list does not display
          * SOLUTION:
@@ -28,19 +26,16 @@ namespace HW6.Controllers
          * In order to diplay the list, go to HW5 to see how to get names out of DB with a for loop
          * To make each result into a link, within the same for loop to display item from DB, use with an HTML.ActionLink
          */
-         [HttpPost]
-         public ActionResult Index(string search)
+        [HttpPost] // for the search, we will use a Post Request here 
+         public ActionResult Index(string search)//In this ActionResult, it needs a paramter that will take in the thing we're searching for
         {
-            var items = db.StockItems.Where(n => n.StockItemName.Contains(search));
-            ViewBag.Success = true;
-            return View(items.ToList());
+            var items = db.StockItems.Where(n => n.StockItemName.Contains(search));// using contains to find the itemname
+            ViewBag.Success = true;// matching in index page ViewBag.Succeess
+            return View(items.ToList());// Return a list
         }
-
-
-        //Don't need About()
         
     }
-}
+
 
 
 
