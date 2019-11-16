76d0957de0826a802e56edd6e43953ccd6002845
Penny <xyang16@wou.edu>
2019-11-16 01:12:34 -0800

    fix the search funtion and get rid off server error

--- HW6/HW6/Controllers/HomeController.cs
+++ HW6/HW6/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace HW6.Controllers
     public class HomeController : Controller
     {
         private WWIContext db = new WWIContext();
-       
+
         // I intially put my return viewn in the StockItemsController which is the wrong controller.
         //This return will not do anything in stockitem controller because this is the initial page (When we first visit the site)
         //return View(db.StockItems.Where(s => s.StockItemName.Contains(name)).ToList());
@@ -26,14 +26,22 @@ namespace HW6.Controllers
          * In order to diplay the list, go to HW5 to see how to get names out of DB with a for loop
          * To make each result into a link, within the same for loop to display item from DB, use with an HTML.ActionLink
          */
-        [HttpPost] // for the search, we will use a Post Request here 
+         [HttpPost] // for the search, we will use a Post Request here 
          public ActionResult Index(string search)//In this ActionResult, it needs a paramter that will take in the thing we're searching for
         {
             var items = db.StockItems.Where(n => n.StockItemName.Contains(search));// using contains to find the itemname
             ViewBag.Success = true;// matching in index page ViewBag.Succeess
             return View(items.ToList());// Return a list
         }
-        
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
     }
 }
 
